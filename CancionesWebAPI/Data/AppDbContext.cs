366321d9b3522229fa8c8c805c6e42b25c0b3dd8
Bismark Abdel Aramayo Lijeron <a2020112817@estudiantes.upsa.edu.bo>
2021-10-12 20:58:12 -0400

    AppDbContext creado

---
+++ CancionesWebAPI/Data/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using CancionesWebAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CancionesWebAPI.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        // Agregar los modelos a utilizar
+        public DbSet<Cancion> Cancion { get; set; }
+    }
+}
